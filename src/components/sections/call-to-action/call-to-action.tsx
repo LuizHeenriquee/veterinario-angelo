f075aa3befceccd535b9a771bc0a29b57406c6a5
Luiz Henrique <127858741+LuizHeenriquee@users.noreply.github.com>
2025-08-28 22:54:08 -0300

    feat: Section Call To Action

---
+++ src/components/sections/call-to-action/call-to-action.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import WhatsAppRedIcon from "../../../assets/wpp-logo-red.png";
+import Image from "next/image";
+
+export function CallToAction() {
+  return (
+    <div className="flex flex-col items-center gap-4 bg-primary text-primary-foreground p-4">
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-center text-2xl font-bold lg:text-4xl">
+          Pronto para garantir a segurança dos seus alimentos?
+        </h2>
+        <p className="text-center text-xl">
+          Entre em contato agora e receba uma consultoria personalizada para
+          suas necessidades.
+        </p>
+      </div>
+      <Link href="#">
+        <Button className="bg-primary-foreground text-primary">
+          <Image src={WhatsAppRedIcon} alt="WhatsApp Icon" width={15} />
+          Receber mais informações no WhatsApp
+        </Button>
+      </Link>
+    </div>
+  );
+}
