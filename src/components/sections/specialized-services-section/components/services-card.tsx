1b2f7eada42ae98cd6a49549a7ada7e3c6a9799c
Luiz Henrique <127858741+LuizHeenriquee@users.noreply.github.com>
2025-08-29 08:15:59 -0300

    feat: Box Shadow no hover do card

--- src/components/sections/specialized-services-section/components/services-card.tsx
+++ src/components/sections/specialized-services-section/components/services-card.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function ServicesCard({ icon: Icon, title, description }: Props) {
   return (
-    <Card className="flex flex-col items-center justify-between h-[238px] lg:w-[250px] lg:h-[266px]">
+    <Card className="flex flex-col items-center justify-between h-[238px] lg:w-[250px] lg:h-[266px] hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <Icon className="text-primary" />
       <h3 className="text-2xl font-bold">{title}</h3>
       <p className="text-center text-lg">{description}</p>
